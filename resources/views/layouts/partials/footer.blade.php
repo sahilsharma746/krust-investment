0f9605b78d70f08b6ff77a181d61a107453ce9f9
Sahil Sharma <sharmasahil00746@gmail.com>
2024-09-05 11:20:54 +0530

    responsive

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="d-flex flex-wrap justify-content-between align-items-start r-g-10">
             <div class="logo-area d-flex align-items-center g-4">
-                <img src="{{ asset('assets') }}/img/site-logo-footer.png" alt="Site Logo" class="site-logo">
+                <img src="./assets/img/site-logo-footer.png" alt="Site Logo" class="site-logo">
                 <span class="site-name">Krust-Investments</span>
             </div>
             <div class="subscribe-area">
                 <p class="text">Subscribe To Our Newsletter</p>
-                <div class="input-area d-flex flex-wrap g-8">
+                <div class="input-area d-flex g-8">
                     <div class="input-group">
                         <input class="form-control" type="email" name="email" id="subscribeEmail"
                             placeholder="Enter email here">
@@ -25,10 +25,9 @@
             </dl>
             <dl>
                 <dt><a href="">Product</a></dt>
-                <dt><a href="{{ route('frontend.accountPlan') }}">Account Plans</a></dt>
-            </dl>
+                <dt><a href="{{ route('frontend.accountPlan') }}">Account Plans</a></dt>            </dl>
             <dl>
-                <dt class="text-end"><a href="{{ route('admin.dashboard') }}">Get started</a></dt>
+                <dt class="text-end"><a href="{{ route('register') }}">Get started</a></dt>
                 <dt class="text-end"><a href="{{ route('login') }}">Log in</a></dt>
                 <dt class="text-end"><a href="{{ route('register') }}">sign up</a></dt>
             </dl>
@@ -36,3 +35,4 @@
         <p class="footer-text text-center">&copy; 2024 Trust Investments. All Rights Reserved.</p>
     </div>
 </footer>
+
