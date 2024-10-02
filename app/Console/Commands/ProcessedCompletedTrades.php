c71ccc963c52966df4e4f43d5ca9535d8ffde575
Sahil Sharma <sharmasahil00746@gmail.com>
2024-10-02 10:06:12 -0400

    updated

--- app/Console/Commands/ProcessedCompletedTrades.php
+++ app/Console/Commands/ProcessedCompletedTrades.php
@@ -26,11 +26,14 @@ class ProcessedCompletedTrades extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
-    {
+    public function handle(){
+
         Log::info("Processed completed trades command ran at " . now());
 
-        $trades = Trade::where('status', '1')->get();
+        // Fetch trades where processed = 0 and status = 1
+        $trades = Trade::where('status', '1')
+                    ->where('processed', '0')
+                    ->get();
 
         foreach ($trades as $trade) {
             $user_id = $trade->user_id;
@@ -46,16 +49,16 @@ class ProcessedCompletedTrades extends Command
 
                 // Log user balance update
                 Log::info("User ID: {$user_id}, New Balance: {$user->balance}, Result: {$user_trade_result}, Amount: {$trade_win_loss_amount}");
-            
+
+                // Mark trade as processed
+                $trade->processed = 1;
+                $trade->save(); // Save the trade to update the processed status
+
             } else {
-            
                 Log::warning("User not found for User ID: {$user_id}");
-            
             }
-        
         }
         
         Log::info("Processed completed trades command ends at " . now() . PHP_EOL);
-
     }
 }
